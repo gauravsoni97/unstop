ac0109497d736dc572a0e565eb74296ba871c025
gauravsoni97 <73748616+gauravsoni97@users.noreply.github.com>
2023-12-28 12:09:10 +0530

    folder structure set

---
+++ src/components/Modal/CreateAssessment.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CreateAssessment = () => {
+  return (
+    <div>CreateAssessment</div>
+  )
+}
+
+export default CreateAssessment
---
+++ src/pages/RoundStatus.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const RoundStatus = () => {
+  return (
+    <div>RoundStatus</div>
+  )
+}
+
+export default RoundStatus
