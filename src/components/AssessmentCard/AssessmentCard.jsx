9c590ec4dcf5989ad8def3c7aade71dd7599e61e
gauravsoni97 <73748616+gauravsoni97@users.noreply.github.com>
2023-12-28 18:18:23 +0530

    cards ui done

---
+++ src/components/AssessmentCard/AssessmentCard.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import joblogo from "../../assets/assessment/joblogo.svg";
+import calendar from "../../assets/assessment/calendar_today.svg";
+import linkbtn from "../../assets/assessment/linkbtn.svg";
+import { Avatar } from "@mui/material";
+import AvatarGroup from "@mui/material/AvatarGroup";
+
+const AssessmentCard = () => {
+  return (
+    <div className="AssessmentCardParent">
+      <div className="AssessmentCardParent_TopSection">
+        <div className="top_icon_more flex-aicjcsb">
+          <div className="top_Card_Icon">
+            <img src={joblogo} alt="" />
+          </div>
+          <span>
+            <MoreVertIcon />
+          </span>
+        </div>
+        <h3 className="card_title">Math Assessment</h3>
+        <div className="jobAbout flex-aic">
+          <span className="jobAboutTxt">Job</span>
+          <span className="divider_jobAboutTxt">|</span>
+          <span className="flex-aic caltextparent">
+            <img src={calendar} alt="" />{" "}
+            <span className="calText"> 20 Apr 2023</span>
+          </span>
+        </div>
+      </div>
+      <div className="AssessmentCardParent_BottomSection flex-aicjcsb">
+        <div className="leftTimer flex-aic">
+          <div className="duration">
+            <span className="durationNum">00</span>
+            <span>Duration</span>
+          </div>
+          <div className="questions">
+            <span className="questionsnum">00</span>
+            <span>Questions</span>
+          </div>
+        </div>
+        <div className="RightShare flex-aic">
+          <button className="linkbtn">
+            <img src={linkbtn} alt="" />
+            Share
+          </button>
+
+          <AvatarGroup max={3}>
+            <Avatar sx={{width:26, height:26, marginRight:"-.5rem", fontSize:".8rem", bgcolor: "#E9407A", padding:".3rem" }}>LP</Avatar>
+            <Avatar sx={{width:26, height:26, marginRight:"-.5rem", fontSize:".8rem", bgcolor: "#3079E1", padding:".3rem" }}>LP</Avatar>
+            <Avatar sx={{width:26, height:26, marginRight:"-.5rem", fontSize:".8rem", bgcolor: "#6548EE", padding:".3rem" }}>LP</Avatar>
+          </AvatarGroup>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AssessmentCard;
--- src/pages/Assessment/MyAssessmentList/MyAssessmentList.jsx
+++ src/pages/Assessment/MyAssessmentList/MyAssessmentList.jsx
@@ -1,9 +1,34 @@
 import React from "react";
-import "../Assessment.css";
+import "./MyAssessmentList.css";
+import AddIcon from '@mui/icons-material/Add';
+import AssessmentCard from "../../../components/AssessmentCard/AssessmentCard";
 const MyAssessmentList = () => {
   return (
     <section className="assessment_sections">
       <h2 className="assessment_heading">My Assessment</h2>
+
+      <div className="cardsListParent">
+        <div className="createNewAssessmentCard flex-aicjcc">
+          <div className="uploadAssessmentCard flex-aicjcc">
+            <div className="plusbtn">
+              <AddIcon/>
+            </div>
+            <h3 className="newAssessment_Heading">New Assessment</h3>
+            <p className="newAssessment_Para">
+              From here you can add questions of multiple types like MCQs,
+              subjective (text or paragraph)!
+            </p>
+          </div>
+        </div>
+          <AssessmentCard />
+          <AssessmentCard />
+          <AssessmentCard />
+          <AssessmentCard />
+          <AssessmentCard />
+          <AssessmentCard />
+          <AssessmentCard />
+          <AssessmentCard />
+      </div>
     </section>
   );
 };
