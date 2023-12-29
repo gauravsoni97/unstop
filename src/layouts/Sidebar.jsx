15e4f39a2275482ec847db9b132bf0572f9084d6
gauravsoni97 <73748616+gauravsoni97@users.noreply.github.com>
2023-12-29 10:12:10 +0530

    set new code

--- src/layouts/Sidebar.jsx
+++ src/layouts/Sidebar.jsx
@@ -1,89 +1,91 @@
 import React, { useEffect, useState } from "react";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
-import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
-import MailIcon from "@mui/icons-material/Mail";
-import { Chip, Divider } from "@mui/material";
-import { MenuList } from "../MenuList";
-import { AdminMenuList } from "../AdminMenuList";
 import { useLocation, useNavigate } from "react-router-dom";
 import "./Sidebar.css";
 
+import { AdminMenuList } from "../AdminMenuList";
+import { MenuList } from "../MenuList";
+
+const SidebarItem = ({ menuItem, isActive, onClick }) => (
+  <ListItem
+    onClick={onClick}
+    sx={{
+      cursor: "pointer",
+      color: isActive ? "#0073E6" : "#E5F1FC",
+      border: isActive ? "2px solid #0073E6" : "transparent",
+      backgroundColor: isActive ? "#E5F1FC" : "transparent",
+      "&:hover": {
+        backgroundColor: "#E5F1FC",
+      },
+      padding: "10px 20px",
+    }}
+    button
+    style={{
+      borderRadius: ".5rem",
+      marginBottom: ".3rem",
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      justifyContent: "center",
+    }}
+  >
+    <ListItemIcon>
+      <img
+        style={{
+          height: "auto",
+          width: "100%",
+          maxWidth: "1.25rem",
+          margin: "0 auto",
+          color: isActive ? "#0073E6" : "#8E92BC",
+        }}
+        src={menuItem?.icon}
+      />
+    </ListItemIcon>
+    <ListItemText
+      primaryTypographyProps={{
+        style: {
+          fontSize: "14px",
+          fontWeight: 600,
+          textAlign: "center",
+          color: isActive ? "#0073E6" : "#8E92BC",
+        },
+      }}
+      primary={menuItem.text}
+    />
+  </ListItem>
+);
+
 const Sidebar = () => {
   const navigate = useNavigate();
-
   const [menuListItem, setMenuListItem] = useState(MenuList);
   const [adminMenuListItem, setAdminMenuListItem] = useState(AdminMenuList);
   const location = useLocation();
   const path = location.pathname.split("/")[1];
 
-  useEffect(() => {}, [path]);
+  const handleItemClick = (item, index, list, setList) => {
+    if (item.child.length === 0) {
+      navigate(item.url);
+    }
+
+    list[index].active = !item.active;
+    setList([...list]);
+  };
 
   return (
     <div>
       <List>
         {menuListItem.map((menuItem, index) => (
-          <ListItem
-            onClick={() => {
-              if (menuItem.child.length === 0) {
-                console.log("Clicked on ListItem", menuItem.url);
-                navigate(menuItem.url);
-              }
-
-              menuListItem[index].active = !menuItem.active;
-              setMenuListItem([...menuListItem]);
-            }}
-            sx={{
-              cursor: "pointer",
-              color: path === menuItem.rootpath ? "#0073E6" : "#E5F1FC",
-              border:
-                path === menuItem.rootpath
-                  ? "2px solid #0073E6"
-                  : "transparent",
-              backgroundColor:
-                path === menuItem.rootpath ? "#E5F1FC" : "transparent",
-              "&:hover": {
-                backgroundColor: "#E5F1FC",
-              },
-              padding: "10px 20px",
-            }}
-            button
-            style={{
-              borderRadius: ".5rem",
-              marginBottom: ".3rem",
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-              justifyContent: "center",
-            }}
-          >
-            <ListItemIcon>
-              <img
-                style={{
-                  height: "auto",
-                  width: "100%",
-                  maxWidth: "1.25rem",
-                  margin: "0 auto",
-                  color: path === menuItem.rootpath ? "#0073E6" : "#8E92BC",
-                  backgroundcolor:
-                    path === menuItem.rootpath ? "#0073E6" : "#8E92BC",
-                }}
-                src={menuItem?.icon}
-              />
-            </ListItemIcon>
-            <ListItemText
-              primaryTypographyProps={{
-                style: {
-                  fontSize: "14px",
-                  fontWeight: 600,
-                  color: path === menuItem.rootpath ? "#0073E6" : "#8E92BC",
-                },
-              }}
-              primary={menuItem.text}
-            />
-          </ListItem>
+          <SidebarItem
+            key={menuItem.text}
+            menuItem={menuItem}
+            isActive={path === menuItem.rootpath}
+            onClick={() =>
+              handleItemClick(menuItem, index, menuListItem, setMenuListItem)
+            }
+          />
         ))}
       </List>
       <center>
@@ -100,73 +102,23 @@ const Sidebar = () => {
       <center>
         <div className="AdminChip">Admin</div>
       </center>
-
-      {adminMenuListItem.map((menuItem, index) => (
-        <ListItem
-          onClick={() => {
-            if (menuItem.child.length === 0) {
-              console.log("Clicked on ListItem", menuItem.url);
-              navigate(menuItem.url);
+      <List>
+        {adminMenuListItem.map((menuItem, index) => (
+          <SidebarItem
+            key={menuItem.text}
+            menuItem={menuItem}
+            isActive={path === menuItem.rootpath}
+            onClick={() =>
+              handleItemClick(
+                menuItem,
+                index,
+                adminMenuListItem,
+                setAdminMenuListItem
+              )
             }
-
-            adminMenuListItem[index].active = !menuItem.active;
-            setAdminMenuListItem([...adminMenuListItem]);
-          }}
-          sx={{
-            cursor: "pointer",
-            color: path === menuItem.rootpath ? "#0073E6" : "#E5F1FC",
-            border:
-              path === menuItem.rootpath ? "2px solid #0073E6" : "transparent",
-            backgroundColor:
-              path === menuItem.rootpath ? "#E5F1FC" : "transparent",
-            "&:hover": {
-              backgroundColor: "#E5F1FC",
-            },
-            padding: "10px 20px",
-          }}
-          button
-          style={{
-            borderRadius: ".5rem",
-            marginBottom: ".3rem",
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            justifyContent: "center",
-          }}
-        >
-          <ListItemIcon
-            style={{
-              maxWidth: "1rem",
-              margin: "auto",
-            }}
-          >
-            <img
-              style={{
-                height: "auto",
-                width: "100%",
-                maxWidth: "1.25rem",
-                margin: "0 auto",
-                color: path === menuItem.rootpath ? "#0073E6" : "#8E92BC",
-                backgroundcolor:
-                  path === menuItem.rootpath ? "#0073E6" : "#8E92BC",
-              }}
-              src={menuItem?.icon}
-            />
-          </ListItemIcon>
-          <ListItemText
-            primaryTypographyProps={{
-              style: {
-                fontSize: "14px",
-                fontWeight: 600,
-                textAlign: "center",
-                // color: "#8E92BC",
-                color: path === menuItem.rootpath ? "#0073E6" : "#8E92BC",
-              },
-            }}
-            primary={menuItem.text}
           />
-        </ListItem>
-      ))}
+        ))}
+      </List>
     </div>
   );
 };
