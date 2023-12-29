84d4c17319704a8f6c8361ba5f6985f20f61bf0a
gauravsoni97 <73748616+gauravsoni97@users.noreply.github.com>
2023-12-29 14:44:24 +0530

    plus modal btn added

--- src/pages/Assessment/MyAssessment.jsx
+++ src/pages/Assessment/MyAssessment.jsx
@@ -2,14 +2,28 @@ import React, { useState } from "react";
 import "./Assessment.css";
 import AssessmentOverview from "./AssessmentOverview/AssessmentOverview";
 import MyAssessmentList from "./MyAssessmentList/MyAssessmentList";
+import AddIcon from "@mui/icons-material/Add";
+
+import { useMediaQuery } from "react-responsive";
 
 const MyAssessment = () => {
   const [showOverview, setShowOverview] = useState(true);
+  const isTabletOrMobile = useMediaQuery({ query: "(max-width: 924px)" });
+
   return (
     <div>
+      {isTabletOrMobile && (
+        <span className="plusbtnModal">
+          <AddIcon />
+        </span>
+      )}
+
       {showOverview && <AssessmentOverview />}
       <br />
-      <MyAssessmentList showOverview={showOverview} setShowOverview={() => setShowOverview(prevState => !prevState)}/>
+      <MyAssessmentList
+        showOverview={showOverview}
+        setShowOverview={() => setShowOverview((prevState) => !prevState)}
+      />
     </div>
   );
 };
